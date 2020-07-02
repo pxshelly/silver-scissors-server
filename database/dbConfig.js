5f2f7f4cd1a0edeee0c0973bfc3130111f482649
Shelly <pxshelly@gmail.com>
2020-07-01 22:06:25 -0700

    Update db config to use heroku db url

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,13 +1,15 @@
-let config = {};
+let dbConfig = {};
 try {
-  config = require('../config');
+  const config = require('../config');
+  dbConfig = {
+    user: process.env.POSTGRES_USER || config.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST || config.POSTGRES_HOST,
+    password: process.env.POSTGRES_PASSWORD || config.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE || config.POSTGRES_DATABASE,
+  };
 } catch(error) {
   //no config found
+  dbConfig.connectionString = process.env.DATABASE_URL;
 }
 
-module.exports = {
-  user: process.env.POSTGRES_USER || config.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST || config.POSTGRES_HOST,
-  password: process.env.POSTGRES_PASSWORD || config.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE || config.POSTGRES_DATABASE,
-};
+module.exports = dbConfig;
